0f324c474b7306d32f7277ce76b663610d31ca94
Piotr Kicki <pkicki@man.poznan.pl>
2017-06-27 12:31:25 +0200

    New AAMConstants added

--- src/main/java/eu/h2020/symbiote/security/constants/AAMConstants.java
+++ src/main/java/eu/h2020/symbiote/security/constants/AAMConstants.java
@@ -10,6 +10,7 @@ public class AAMConstants {
     // AAM GLOBAL
     public static final String AAM_CORE_AAM_FRIENDLY_NAME = "SymbIoTe Core AAM";
     public static final String AAM_CORE_AAM_INSTANCE_ID = "SymbIoTe_Core_AAM";
+
     // AAM AMQP
     public static final String AAM_EXCHANGE_NAME = "symbIoTe.AuthenticationAuthorizationManager";
     public static final String AAM_CHECK_REVOCATION_QUEUE =
@@ -20,14 +21,17 @@ public class AAMConstants {
             "symbIoTe-AuthenticationAuthorizationManager-login_request";
     public static final String AAM_LOGIN_ROUTING_KEY = "symbIoTe.AuthenticationAuthorizationManager" +
             ".login_request";
+
     // AAM REST
+    public static final String AAM_ADMIN_PATH = "/admin";
     public static final String AAM_CHECK_HOME_TOKEN_REVOCATION = "/check_home_token_revocation";
     public static final String AAM_GET_AVAILABLE_AAMS = "/get_available_aams";
     public static final String AAM_GET_CA_CERTIFICATE = "/get_ca_cert";
     public static final String AAM_LOGIN = "/login";
+    public static final String AAM_PUBLIC_PATH = "/public";
     public static final String AAM_REQUEST_FOREIGN_TOKEN = "/request_foreign_token";
-    // errors
 
+    // errors
     public static final String ERR_MISSING_ARGUMENTS = "ERR_MISSING_ARGUMENTS";
     public static final String ERR_TOKEN_EXPIRED = "TOKEN_EXPIRED";
     public static final String ERR_WRONG_CREDENTIALS = "ERR_WRONG_CREDENTIALS";
