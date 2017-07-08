87482ecd763949114a86d9b925a68eecbe267618
Maksymilian Marcinowski <ksymilian@man.poznan.pl>
2017-07-08 18:27:31 +0200

    registration responses changed

--- src/main/java/eu/h2020/symbiote/security/payloads/PlatformRegistrationResponse.java
+++ src/main/java/eu/h2020/symbiote/security/payloads/PlatformRegistrationResponse.java
@@ -1,6 +1,6 @@
 package eu.h2020.symbiote.security.payloads;
 
-import eu.h2020.symbiote.security.certificate.Certificate;
+import eu.h2020.symbiote.security.enums.RegistrationStatus;
 
 /**
  * Describes a response for platform registration sent by AAM
@@ -9,41 +9,16 @@ import eu.h2020.symbiote.security.certificate.Certificate;
  * @author Mikołaj Dobski (PSNC)
  */
 public class PlatformRegistrationResponse {
-    private Certificate platformOwnerCertificate = new Certificate();
-    private String platformOwnerPrivateKey = "";
     private String platformId = "";
+    private RegistrationStatus registrationStatus;
 
     public PlatformRegistrationResponse() {
         // used by serializer
     }
 
-    public PlatformRegistrationResponse(Certificate platformOwnerCertificate, String platformOwnerPrivateKey, String
-            registeredPlatformId) {
-        this.platformOwnerCertificate = platformOwnerCertificate;
-        this.platformOwnerPrivateKey = platformOwnerPrivateKey;
+    public PlatformRegistrationResponse(String registeredPlatformId, RegistrationStatus registrationStatus) {
         this.platformId = registeredPlatformId;
-    }
-
-    /**
-     * @return Platform Owner certificate to be used for interaction with symbIoTe core components.
-     */
-    public Certificate getPlatformOwnerCertificate() {
-        return platformOwnerCertificate;
-    }
-
-    public void setPlatformOwnerCertificate(Certificate platformOwnerCertificate) {
-        this.platformOwnerCertificate = platformOwnerCertificate;
-    }
-
-    /**
-     * @return in PEM format
-     */
-    public String getPlatformOwnerPrivateKey() {
-        return platformOwnerPrivateKey;
-    }
-
-    public void setPlatformOwnerPrivateKey(String platformOwnerPrivateKey) {
-        this.platformOwnerPrivateKey = platformOwnerPrivateKey;
+        this.registrationStatus = registrationStatus;
     }
 
     /**
@@ -55,5 +30,13 @@ public class PlatformRegistrationResponse {
 
     public void setPlatformId(String platformId) {
         this.platformId = platformId;
+    }
+
+    public RegistrationStatus getRegistrationStatus() {
+        return registrationStatus;
+    }
+
+    public void setRegistrationStatus(RegistrationStatus registrationStatus) {
+        this.registrationStatus = registrationStatus;
     }
 }
--- src/main/java/eu/h2020/symbiote/security/payloads/UserRegistrationResponse.java
+++ src/main/java/eu/h2020/symbiote/security/payloads/UserRegistrationResponse.java
@@ -2,7 +2,7 @@ package eu.h2020.symbiote.security.payloads;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import eu.h2020.symbiote.security.certificate.Certificate;
+import eu.h2020.symbiote.security.enums.RegistrationStatus;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -18,8 +18,7 @@ public class UserRegistrationResponse {
     private static Log log = LogFactory.getLog(UserRegistrationResponse.class);
 
     // TODO Release 3 fix to support CertificateSignRequests
-    private Certificate userCertificate = new Certificate();
-    private String userPrivateKey = "";
+    private RegistrationStatus registrationStatus;
 
     /**
      * required for JSON serialization
@@ -28,31 +27,16 @@ public class UserRegistrationResponse {
         // required by JSON
     }
 
-    public UserRegistrationResponse(Certificate userCertificate, String userPrivateKey) {
-        this.userCertificate = userCertificate;
-        this.userPrivateKey = userPrivateKey;
+    public UserRegistrationResponse(RegistrationStatus registrationStatus) {
+        this.registrationStatus = registrationStatus;
     }
 
-    /**
-     * @return user certificate to be used for interaction with symbIoTe core components.
-     */
-    public Certificate getUserCertificate() {
-        return userCertificate;
+    public RegistrationStatus getRegistrationStatus() {
+        return registrationStatus;
     }
 
-    public void setUserCertificate(Certificate userCertificate) {
-        this.userCertificate = userCertificate;
-    }
-
-    /**
-     * @return in PEM format
-     */
-    public String getUserPrivateKey() {
-        return userPrivateKey;
-    }
-
-    public void setUserPrivateKey(String userPrivateKey) {
-        this.userPrivateKey = userPrivateKey;
+    public void setRegistrationStatus(RegistrationStatus registrationStatus) {
+        this.registrationStatus = registrationStatus;
     }
 
     public String toJson() {
@@ -64,5 +48,6 @@ public class UserRegistrationResponse {
             return null;
         }
     }
+
 
 }
