8bda8e1d642fc3c04fc71d1bd20abc83a0873001
Mikołaj Dobski <mikolaj.dobski@man.poznan.pl>
2017-08-16 12:36:15 +0200

    extracted RESTAAMClient interface as IAAMClient for easier mocking and hid the implementation in the AAMClient.java

--- src/main/java/eu/h2020/symbiote/security/communication/AAMClient.java
+++ src/main/java/eu/h2020/symbiote/security/communication/AAMClient.java
@@ -4,7 +4,7 @@ import eu.h2020.symbiote.security.commons.SecurityConstants;
 import eu.h2020.symbiote.security.commons.credentials.HomeCredentials;
 import eu.h2020.symbiote.security.commons.enums.ValidationStatus;
 import eu.h2020.symbiote.security.commons.exceptions.custom.*;
-import eu.h2020.symbiote.security.communication.interfaces.FeignAAMRESTInterface;
+import eu.h2020.symbiote.security.communication.interfaces.IFeignAAMClient;
 import eu.h2020.symbiote.security.communication.payloads.AvailableAAMsCollection;
 import eu.h2020.symbiote.security.communication.payloads.CertificateRequest;
 import feign.Feign;
@@ -15,36 +15,37 @@ import feign.jackson.JacksonEncoder;
 import java.util.Optional;
 
 /**
- * Crude RMI-like client to the AAM module that communicates with it over REST.
+ * Crude RMI-like client's implementation to the AAM module that communicates with it over REST.
  *
  * @author Dariusz Krajewski (PSNC)
  */
-public class RESTAAMClient {
+public class AAMClient implements IAAMClient {
 
     private String serverAddress;
-    private FeignAAMRESTInterface aamClient;
+    private IFeignAAMClient feignClient;
 
     /**
      * @param serverAddress of the AAM server the client wants to interact with.
      */
-    public RESTAAMClient(String serverAddress) {
+    public AAMClient(String serverAddress) {
         this.serverAddress = serverAddress;
-        this.aamClient = getJsonClient();
+        this.feignClient = getJsonClient();
     }
 
     /**
      * @return Instance of feign client with all necessary parameters set
      */
-    private FeignAAMRESTInterface getJsonClient() {
+    private IFeignAAMClient getJsonClient() {
         return Feign.builder().encoder(new JacksonEncoder()).decoder(new JacksonDecoder())
-                .target(FeignAAMRESTInterface.class, serverAddress);
+                .target(IFeignAAMClient.class, serverAddress);
     }
 
     /**
      * @return Certificate of the component in PEM format. In this case the AAM certificate.
      */
+    @Override
     public String getComponentCertificate() throws AAMException {
-        Response response = aamClient.getComponentCertificate();
+        Response response = feignClient.getComponentCertificate();
         if (response.status() == 500)
             throw new AAMException(response.body().toString());
         return response.body().toString();
@@ -56,12 +57,13 @@ public class RESTAAMClient {
      * @param certificateRequest required to issue a certificate for given (username, clientId) tupple.
      * @return the signed certificate from the provided CSR in PEM format
      */
+    @Override
     public String getClientCertificate(CertificateRequest certificateRequest) throws
             WrongCredentialsException,
             NotExistingUserException,
             ValidationException,
             InvalidArgumentsException {
-        Response response = aamClient.getClientCertificate(certificateRequest);
+        Response response = feignClient.getClientCertificate(certificateRequest);
         switch (response.status()) {
             case 400:
                 if (response.body().toString().contains("INVALID_ARGUMENTS"))
@@ -77,8 +79,9 @@ public class RESTAAMClient {
     /**
      * @return GUEST token used to access public resources offered in SymbIoTe
      */
+    @Override
     public String getGuestToken() throws JWTCreationException {
-        Response response = aamClient.getGuestToken();
+        Response response = feignClient.getGuestToken();
         if (response.status() == 500)
             throw new JWTCreationException("Server failed to create a guest token");
         return response.headers().get(SecurityConstants.TOKEN_HEADER_NAME).toString();
@@ -89,11 +92,12 @@ public class RESTAAMClient {
      *                     and http://www.smarteremc2.eu/colab/display/SYM/Home+Authorization+Token+acquisition+%28home+login%29+request
      * @return HOME token used to access restricted resources offered in SymbIoTe
      */
+    @Override
     public String getHomeToken(String loginRequest) throws
             WrongCredentialsException,
             JWTCreationException,
             MalformedJWTException {
-        Response response = aamClient.getHomeToken(loginRequest);
+        Response response = feignClient.getHomeToken(loginRequest);
         switch (response.status()) {
             case 400:
                 throw new MalformedJWTException("Unable to read malformed token");
@@ -111,10 +115,11 @@ public class RESTAAMClient {
      * @param aamCertificate    in PEM with key matching the IPK claim in the provided token in 'offline' (intranet) scenarios
      * @return FOREIGN token used to access restricted resources offered in SymbIoTe federations
      */
+    @Override
     public String getForeignToken(String remoteHomeToken, Optional<String> clientCertificate, Optional<String> aamCertificate) throws
             ValidationException,
             JWTCreationException {
-        Response response = aamClient.getForeignToken(remoteHomeToken, clientCertificate.orElse(""), aamCertificate.orElse(""));
+        Response response = feignClient.getForeignToken(remoteHomeToken, clientCertificate.orElse(""), aamCertificate.orElse(""));
         switch (response.status()) {
             case 401:
                 throw new ValidationException("Failed to validate homeToken");
@@ -127,8 +132,9 @@ public class RESTAAMClient {
     /**
      * @return collection of AAMs available in the SymbIoTe ecosystem
      */
+    @Override
     public AvailableAAMsCollection getAvailableAAMs() {
-        return aamClient.getAvailableAAMs();
+        return feignClient.getAvailableAAMs();
     }
 
     /**
@@ -137,8 +143,9 @@ public class RESTAAMClient {
      * @param aamCertificate    in PEM with key matching the IPK claim in the provided token in 'offline' (intranet) scenarios
      * @return validation status
      */
+    @Override
     public ValidationStatus validate(String token, Optional<String> clientCertificate, Optional<String> aamCertificate) {
-        return aamClient.validate(token, clientCertificate.orElse(""), aamCertificate.orElse(""));
+        return feignClient.validate(token, clientCertificate.orElse(""), aamCertificate.orElse(""));
     }
 
 }
---
+++ src/main/java/eu/h2020/symbiote/security/communication/IAAMClient.java
@@ -0,0 +1,72 @@
+package eu.h2020.symbiote.security.communication;
+
+import eu.h2020.symbiote.security.commons.credentials.HomeCredentials;
+import eu.h2020.symbiote.security.commons.enums.ValidationStatus;
+import eu.h2020.symbiote.security.commons.exceptions.custom.*;
+import eu.h2020.symbiote.security.communication.payloads.AvailableAAMsCollection;
+import eu.h2020.symbiote.security.communication.payloads.CertificateRequest;
+
+import java.util.Optional;
+
+/**
+ * Crude RMI-like client's interface to the AAM module.
+ *
+ * @author Dariusz Krajewski (PSNC)
+ * @author Mikolaj Dobski (PSNC)
+ */
+public interface IAAMClient {
+    /**
+     * @return Certificate of the component in PEM format. In this case the AAM certificate.
+     */
+    String getComponentCertificate() throws AAMException;
+
+    /**
+     * Allows the user to acquire their client's certificate.
+     *
+     * @param certificateRequest required to issue a certificate for given (username, clientId) tupple.
+     * @return the signed certificate from the provided CSR in PEM format
+     */
+    String getClientCertificate(CertificateRequest certificateRequest) throws
+            WrongCredentialsException,
+            NotExistingUserException,
+            ValidationException,
+            InvalidArgumentsException;
+
+    /**
+     * @return GUEST token used to access public resources offered in SymbIoTe
+     */
+    String getGuestToken() throws JWTCreationException;
+
+    /**
+     * @param loginRequest JWS build in accordance to @{@link eu.h2020.symbiote.security.helpers.CryptoHelper#buildHomeTokenAcquisitionRequest(HomeCredentials)}
+     *                     and http://www.smarteremc2.eu/colab/display/SYM/Home+Authorization+Token+acquisition+%28home+login%29+request
+     * @return HOME token used to access restricted resources offered in SymbIoTe
+     */
+    String getHomeToken(String loginRequest) throws
+            WrongCredentialsException,
+            JWTCreationException,
+            MalformedJWTException;
+
+    /**
+     * @param remoteHomeToken   that an actor wants to exchange in this AAM for a FOREIGN token
+     * @param clientCertificate in PEM with key matching the SPK claim in the provided token in 'offline' (intranet) scenarios
+     * @param aamCertificate    in PEM with key matching the IPK claim in the provided token in 'offline' (intranet) scenarios
+     * @return FOREIGN token used to access restricted resources offered in SymbIoTe federations
+     */
+    String getForeignToken(String remoteHomeToken, Optional<String> clientCertificate, Optional<String> aamCertificate) throws
+            ValidationException,
+            JWTCreationException;
+
+    /**
+     * @return collection of AAMs available in the SymbIoTe ecosystem
+     */
+    AvailableAAMsCollection getAvailableAAMs();
+
+    /**
+     * @param token             that is to be validated
+     * @param clientCertificate in PEM with key matching the SPK claim in the provided token in 'offline' (intranet) scenarios
+     * @param aamCertificate    in PEM with key matching the IPK claim in the provided token in 'offline' (intranet) scenarios
+     * @return validation status
+     */
+    ValidationStatus validate(String token, Optional<String> clientCertificate, Optional<String> aamCertificate);
+}
