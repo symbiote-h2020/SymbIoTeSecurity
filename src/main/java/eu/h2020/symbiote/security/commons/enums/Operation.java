b7aea0c3df9085dfa2cdc9e0d61dddba28faf391
Maksymilian Marcinowski <ksymilian@man.poznan.pl>
2017-08-17 09:05:11 +0200

    operation added

---
+++ src/main/java/eu/h2020/symbiote/security/commons/enums/Operation.java
@@ -0,0 +1,16 @@
+package eu.h2020.symbiote.security.commons.enums;
+
+public enum Operation {
+    /**
+     * creating actor
+     */
+    CREATE,
+    /**
+     * updating actor
+     */
+    UPDATE,
+    /**
+     * deleting actor
+     */
+    DELETE,
+}
