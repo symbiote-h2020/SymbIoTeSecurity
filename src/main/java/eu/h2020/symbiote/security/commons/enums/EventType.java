35c5e8db7f4e2584a4d13f5ab08954a1533d2803
Jakub Toczek <jakub.toczek@man.poznan.pl>
2017-10-26 12:58:45 +0200

    Event time expanded.

--- src/main/java/eu/h2020/symbiote/security/commons/enums/EventType.java
+++ src/main/java/eu/h2020/symbiote/security/commons/enums/EventType.java
@@ -2,10 +2,21 @@ package eu.h2020.symbiote.security.commons.enums;
 
 public enum EventType {
 
+    /**
+     * Error during token validation occurred.
+     */
     VALIDATION_FAILED,
-
+    /**
+     * Error during User Management occurred due to wrong credentials.
+     */
     LOGIN_FAILED,
-
+    /**
+     * Error during token acquisition occurred.
+     */
     ACQUISITION_FAILED,
+    /**
+     * used only in default constructor of the EventLog.
+     */
+    NULL,
 
 }
