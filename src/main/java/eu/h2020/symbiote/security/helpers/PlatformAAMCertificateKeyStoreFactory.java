674b6099375c08cd6a752ad24a1d485041d028bd
Jakub Toczek <jakub.toczek@man.poznan.pl>
2018-01-25 16:16:26 +0100

    Fix in checking certificate (not to use cached values)

--- src/main/java/eu/h2020/symbiote/security/helpers/PlatformAAMCertificateKeyStoreFactory.java
+++ src/main/java/eu/h2020/symbiote/security/helpers/PlatformAAMCertificateKeyStoreFactory.java
@@ -3,7 +3,10 @@ package eu.h2020.symbiote.security.helpers;
 import eu.h2020.symbiote.security.commons.Certificate;
 import eu.h2020.symbiote.security.commons.SecurityConstants;
 import eu.h2020.symbiote.security.commons.exceptions.SecurityException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.*;
+import eu.h2020.symbiote.security.commons.exceptions.custom.AAMException;
+import eu.h2020.symbiote.security.commons.exceptions.custom.InvalidArgumentsException;
+import eu.h2020.symbiote.security.commons.exceptions.custom.NotExistingUserException;
+import eu.h2020.symbiote.security.commons.exceptions.custom.ValidationException;
 import eu.h2020.symbiote.security.communication.AAMClient;
 import eu.h2020.symbiote.security.communication.payloads.CertificateRequest;
 import org.apache.commons.logging.Log;
@@ -28,49 +31,49 @@ public class PlatformAAMCertificateKeyStoreFactory {
 
     /**
      * Generates a platform AAM keystore.
-     * 
+     * <p>
      * Fill properly all the fields in as in the template 'cert.properties' to get the platform AAM keystore.
      * You can send as first argument the name of the file if it is nod default name. If you do not provide default
      * filename the it uses 'cert.properties'.
-     * 
+     * <p>
      * You need to build all jar with gradle task: buildRunnablePAAMKeystoreFactoryJAR
-     *  
-     * After that you can start it e.g., java -jar build/libs/SymbIoTeSecurity-all-23.0.2.jar 
+     * <p>
+     * After that you can start it e.g., java -jar build/libs/SymbIoTeSecurity-all-23.0.2.jar
      */
     public static void main(String[] args) {
-    		Properties props = new Properties();
-		if(args.length == 1) {
-			try {
-				props.load(new FileReader(args[0]));
-			} catch (IOException e) {
-				System.err.println("Can not load properties file '" + args[0] + "'. Reason: " + e);
-				System.exit(1);
-			}
-		} else {
-			try {
-				props.load(new FileReader("cert.properties"));
-			} catch (IOException e) {
-				System.err.println("Can not load properties file 'cert.properties'. Reason: " + e);
-				System.exit(2);
-			}
-		}
-    		
+        Properties props = new Properties();
+        if (args.length == 1) {
+            try {
+                props.load(new FileReader(args[0]));
+            } catch (IOException e) {
+                System.err.println("Can not load properties file '" + args[0] + "'. Reason: " + e);
+                System.exit(1);
+            }
+        } else {
+            try {
+                props.load(new FileReader("cert.properties"));
+            } catch (IOException e) {
+                System.err.println("Can not load properties file 'cert.properties'. Reason: " + e);
+                System.exit(2);
+            }
+        }
+
         // given to you for integration, in the end should be available in public
         // from spring bootstrap file: symbIoTe.core.interface.url
-        String coreAAMAddress = (String) props.computeIfAbsent("coreAAMAddress", 
-        		(p) -> exit("Property 'coreAAMAddress' can not be absent."));
+        String coreAAMAddress = (String) props.computeIfAbsent("coreAAMAddress",
+                (p) -> exit("Property 'coreAAMAddress' can not be absent."));
         // of the user registered through administration in the symbIoTe Core
-        String platformOwnerUsername = (String) props.computeIfAbsent("platformOwnerUsername", 
-        		(p) -> exit("Property 'platformOwnerUsername' can not be absent."));
-        String platformOwnerPassword = (String) props.computeIfAbsent("platformOwnerPassword", 
-        		(p) -> exit("Property 'platformOwnerPassword' can not be absent."));
+        String platformOwnerUsername = (String) props.computeIfAbsent("platformOwnerUsername",
+                (p) -> exit("Property 'platformOwnerUsername' can not be absent."));
+        String platformOwnerPassword = (String) props.computeIfAbsent("platformOwnerPassword",
+                (p) -> exit("Property 'platformOwnerPassword' can not be absent."));
         // of the platform registered to the given platform Owner
-        String platformId = (String) props.computeIfAbsent("platformId", 
-        		(p) -> exit("Property 'platformId' can not be absent."));
+        String platformId = (String) props.computeIfAbsent("platformId",
+                (p) -> exit("Property 'platformId' can not be absent."));
 
         // how the generated keystore should be named
-        String keyStoreFileName = (String) props.computeIfAbsent("keyStoreFileName", 
-        		(p) -> exit("Property keyStoreFileName anc not be absent."));
+        String keyStoreFileName = (String) props.computeIfAbsent("keyStoreFileName",
+                (p) -> exit("Property keyStoreFileName anc not be absent."));
         // used to access the keystore. MUST NOT be longer than 7 chars
         // from spring bootstrap file: aam.security.KEY_STORE_PASSWORD
         // R3 dirty fix MUST BE THE SAME as spring bootstrap file: aam.security.PV_KEY_PASSWORD
@@ -105,11 +108,11 @@ public class PlatformAAMCertificateKeyStoreFactory {
         }
     }
 
-	private static String exit(String msg) {
-		System.err.println(msg); 
-			System.exit(3);
-			return null;
-	}
+    private static String exit(String msg) {
+        System.err.println(msg);
+        System.exit(3);
+        return null;
+    }
 
     public static void getPlatformAAMKeystore(String coreAAMAddress,
                                               String platformOwnerUsername,
@@ -127,7 +130,6 @@ public class PlatformAAMCertificateKeyStoreFactory {
             NoSuchProviderException,
             InvalidAlgorithmParameterException,
             InvalidArgumentsException,
-            WrongCredentialsException,
             NotExistingUserException,
             ValidationException,
             AAMException {
@@ -153,7 +155,7 @@ public class PlatformAAMCertificateKeyStoreFactory {
         log.info("Connection with AAMClient established");
         String platformAAMCertificate = aamClient.signCertificateRequest(request);
         log.info("Platform Certificate acquired");
-        if (!aamClient.getAvailableAAMs().getAvailableAAMs().get(platformId).getAamCACertificate().getCertificateString().equals(platformAAMCertificate)) {
+        if (!aamClient.getComponentCertificate(SecurityConstants.AAM_COMPONENT_NAME, platformId).equals(platformAAMCertificate)) {
             throw new CertificateException("Wrong certificate under the platformId");
         }
         // rootCA part
