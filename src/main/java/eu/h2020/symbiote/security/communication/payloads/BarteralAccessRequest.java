bd336ce49b65661e926830a5d081845f172ee6a5
Jakub Toczek <jakub.toczek@man.poznan.pl>
2018-05-14 15:09:35 +0200

    Payloads update

--- src/main/java/eu/h2020/symbiote/security/communication/payloads/BarteralAccessRequest.java
+++ src/main/java/eu/h2020/symbiote/security/communication/payloads/BarteralAccessRequest.java
@@ -1,4 +1,35 @@
 package eu.h2020.symbiote.security.communication.payloads;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.h2020.symbiote.security.commons.Coupon;
+
 public class BarteralAccessRequest {
+
+    private final String clientId;
+    private final String resourceId;
+    private final Coupon.Type couponType;
+
+    @JsonCreator
+    public BarteralAccessRequest(@JsonProperty("clientId") String clientId,
+                                 @JsonProperty("resourceId") String resourceId,
+                                 @JsonProperty("couponType") Coupon.Type couponType) {
+        this.clientId = clientId;
+        this.resourceId = resourceId;
+        this.couponType = couponType;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public Coupon.Type getCouponType() {
+        return couponType;
+    }
+
+
 }
--- src/main/java/eu/h2020/symbiote/security/communication/payloads/CouponRequest.java
+++ src/main/java/eu/h2020/symbiote/security/communication/payloads/CouponRequest.java
@@ -1,4 +1,33 @@
 package eu.h2020.symbiote.security.communication.payloads;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.h2020.symbiote.security.commons.Coupon;
+
 public class CouponRequest {
+
+    private final Coupon.Type couponType;
+    private final String platformId;
+    private final SecurityRequest securityRequest;
+
+    @JsonCreator
+    public CouponRequest(@JsonProperty("couponType") Coupon.Type couponType,
+                         @JsonProperty("platformId") String platformId,
+                         @JsonProperty("securityRequest") SecurityRequest securityRequest) {
+        this.couponType = couponType;
+        this.platformId = platformId;
+        this.securityRequest = securityRequest;
+    }
+
+    public Coupon.Type getCouponType() {
+        return couponType;
+    }
+
+    public String getPlatformId() {
+        return platformId;
+    }
+
+    public SecurityRequest getSecurityRequest() {
+        return securityRequest;
+    }
 }
